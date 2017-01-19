c42152c125b17d90216fa1edafd4eb478c97509e
Michael Butkiewicz <mbutki@gmail.com>
2017-01-18 22:47:15 -0800

    fix temperature db connection code

--- temperatureRead/temp.py
+++ temperatureRead/temp.py
@@ -11,9 +11,10 @@ SPI_DEVICE = 0
 mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 READ_FREQ_SECS = 1
 WRITE_FREQ_SECS = 1 * 60 * 10
-LOCATION = '"desk"'
+LOCATION = pi_config.location
 
 db_config = json.load(open('/home/mbutki/pi_projects/db.config'))
+pi_config = json.load(open('/home/mbutki/pi_projects/pi.config'))
 
 data = []
 while True:
@@ -24,17 +25,19 @@ while True:
     temp_F = ((temp_C + 1.3) * 9.0 / 5.0) + 32
     if len(data) < WRITE_FREQ_SECS:
         data.append(temp_F)
-        print temp_F
     else:
         median = numpy.median(numpy.array(data))
         temp = int(round(median))
         data = []
 
-        db = MySQLdb.connect(db_config)
+        db = MySQLdb.connect(host=db_config['host'],
+                             user=db_config['user'],
+                             passwd=db_config['password'],
+                             db=db_config['database'])
+
         cur = db.cursor()
         try:
             cmd = 'insert into temperature values(NOW(), {0}, {1});'.format(LOCATION, temp)
-            print cmd
             cur.execute(cmd)
             db.commit()
         except Exception as e:
