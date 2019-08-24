a642c18d56c0a97696d3e6f6d58a661c05e2da15
Michael Butkiewicz <mbutki@gmail.com>
2019-08-23 17:53:47 -0700

    fix on/off weather button

--- weather/showWeatherButton.py
+++ weather/showWeatherButton.py
@@ -3,12 +3,17 @@ import time
 import sys
 import subprocess
 import json
+import argparse
+
+parser = argparse.ArgumentParser(description='Display Weather')
+parser.add_argument('-v', default=False, action='store_true', help='verbose mode')
+args = parser.parse_args()
 
 pi_config = json.load(open('/home/mbutki/pi_projects/pi.config'))
 PIN_IN = pi_config['weather_display_button_pin']
 
 gpio.setmode(gpio.BCM)
-gpio.setup(PIN_IN, gpio.IN, pull_up_down=gpio.PUD_DOWN)
+gpio.setup(PIN_IN, gpio.IN, pull_up_down=gpio.PUD_UP)
 
 restartService = 'sudo service showWeather restart'
 stopService = 'sudo service showWeather stop'
@@ -16,15 +21,19 @@ displayOn = True
 
 try:
     while True:
-        gpio.wait_for_edge(PIN_IN, gpio.RISING, bouncetime=200)
+        gpio.wait_for_edge(PIN_IN, gpio.FALLING, bouncetime=200)
         if displayOn:
-            print 'turning off'
+            if args.v:
+                print 'turning off'
             subprocess.call(stopService, shell = True)
-            print 'turned off'
+            if args.v:
+                print 'turned off'
         else:
-            print 'turning on'
+            if args.v:
+                print 'turning on'
             subprocess.call(restartService, shell = True)
-            print 'turned on'
+            if args.v:
+                print 'turned on'
         displayOn = not displayOn
 except KeyboardInterrupt:
     print '\nExiting'
