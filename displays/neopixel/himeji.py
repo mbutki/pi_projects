4fc8197ebd893d7696f3ad0194f6b1ae8b8a026d
Michael Butkiewicz <mbutki@gmail.com>
2019-11-23 17:18:24 -0800

    aaa

--- displays/neopixel/himeji.py
+++ displays/neopixel/himeji.py
@@ -11,6 +11,7 @@ pixelPin = board.D21
 
 numPixels = 30
 num = 1
+stepMax = 5
 
 pixels = neopixel.NeoPixel(pixelPin, numPixels, brightness=0.05, auto_write=False)
 
@@ -28,13 +29,42 @@ def translate(value, leftMin, leftMax, rightMin, rightMax):
 class Torch():
     hRange = [30, 40, 360]
     sRange = [99, 100, 100]
-    vRange = [60, 100, 100]
+    vRange = [50, 100, 100]
+
+    steps = 0
+    nextColor = (0, 0, 0)
+    prevColor = (0, 0, 0)
+    curColor = (0, 0, 0)
+    colorSteps = ()
 
     def __init__(self, pos):
         self.pos = pos
     
     def draw(self):
-        pixels[self.pos] = self.genColor()
+        if self.steps == 0:
+            self.steps = stepMax
+            #self.prevColor = self.nextColor
+            self.prevColor = self.curColor
+            self.nextColor = self.genColor()
+            self.colorSteps = [int((x - self.prevColor[i]) / stepMax) for i, x in enumerate(self.nextColor)]
+        
+        self.steps -= 1
+        self.curColor = [x + self.curColor[i] for i, x in enumerate(self.colorSteps)]
+        self.curColor = [max(x, 0) for x in self.curColor]
+        self.curColor = [min(x, 255) for x in self.curColor]
+
+        '''        
+        print('\n')
+        print('self.pos:{}'.format(self.pos))
+        print('self.steps:{}'.format(self.steps))
+        print('self.prevColor:{}'.format(self.prevColor))
+        print('self.nextColor:{}'.format(self.nextColor))
+        print('self.curColor:{}'.format(self.curColor))
+        print('self.colorSteps:{}'.format(self.colorSteps))
+        #pixels[self.pos] = self.genColor()
+        '''
+
+        pixels[self.pos] = self.curColor
 
     def genColor(self):
         (r, g, b) = colorsys.hsv_to_rgb(self.genVal(self.hRange),
@@ -52,7 +82,7 @@ class Torch():
         for i in range(50):
             self.draw()
             pixels.show()
-            sleep(0.030)
+            sleep(0.01)
 
     def static(self):
         self.draw()
@@ -62,19 +92,22 @@ def main():
     #try:
     torches = []
     #for i in range(num):
-    torches.append(Torch(0))
+    #torches.append(Torch(0))
     torches.append(Torch(15))
     torches.append(Torch(20))
+    torches.append(Torch(26))
     torches.append(Torch(27))
 
+    pixels.fill((0, 0, 0))
+    pixels.show()
     while True:
-        pixels.fill((0, 0, 0))
-        pixels.show()
         #torches[0].walk()
+        torches[0].static()
         torches[1].static()
         torches[2].static()
         torches[3].static()
-        sleep(0.05)
+        #torches[4].static()
+        sleep(0.03)
     '''
     except Exception as e:
         print(e)
