d51f0b3b5ce9245ed5aa1b957a1e487b5d6f77d5
Michael Butkiewicz <mbutki@gmail.com>
2019-12-31 14:18:15 -0800

    from vscode

--- piFrontend/pi-app/app/models/graph.js
+++
@@ -1,7 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  location: DS.attr('string'),
-  times: DS.attr(),
-  values: DS.attr()
-});
--- piFrontend/pi-app/build.sh
+++
@@ -1,7 +0,0 @@
-ember build
-#ember build --environment=production
-rm -rf ~/pi_projects/piWebserver/assets
-rm ~/pi_projects/piWebserver/service-worker-*
-rm ~/pi_projects/piWebserver/sw-toolbox-*
-
-cp -r dist/* ~/pi_projects/piWebserver/
