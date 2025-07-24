a7059e7f4249ab88e1df65372637aa8d5f489578
Michael Butkiewicz <mbutki@gmail.com>
2025-07-24 13:31:13 -0700

    blank messsage

--- python/src/displays/eink_weather/systemd/setup_units.sh
+++ python/src/displays/eink_weather/systemd/setup_units.sh
@@ -1,9 +1,9 @@
-sudo cp eink-weather-manga.service /etc/systemd/system/
-sudo cp eink-weather-manga.timer /etc/systemd/system/
+sudo cp eink_weather_manga.service /etc/systemd/system/
+sudo cp eink_weather_manga.timer /etc/systemd/system/
 
 sudo systemctl daemon-reload
 
-sudo systemctl start eink-weather-manga.timer
-sudo systemctl enable eink-weather-manga.timer
+sudo systemctl start eink_weather_manga.timer
+sudo systemctl enable eink_weather_manga.timer
 
 
