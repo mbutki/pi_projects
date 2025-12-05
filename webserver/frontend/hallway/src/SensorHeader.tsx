e3d751f45b266684400a39be213ec5ade18ef77f
Michael Butkiewicz <mbutki@gmail.com>
2025-12-05 13:28:00 -0800

    refactor: remove hallway package and related assets; implement SensorHeader component
    - Deleted hallway package.json, postcss.config.mjs, tsconfig.json, and SVG assets.
    - Refactored index.tsx to utilize SensorHeader for tab navigation.
    - Added styles for active buttons in sensors.
    - Created SensorHeader component for better tab management.

---
+++ webserver/frontend/hallway/src/SensorHeader.tsx
@@ -0,0 +1,11 @@
+function SensorHeader({ tab, onClick }: { tab: string, onClick: (tabName: string) => void }) {
+    return (
+        <div className='sensors'>
+            <button onClick={() => { onClick('live') }} className={tab === 'live' ? 'active' : ''}>Live</button>
+            <button onClick={() => { onClick('errors') }} className={tab === 'errors' ? 'active' : ''}>Errors</button>
+            <button onClick={() => { onClick('graphs') }} className={tab === 'graphs' ? 'active' : ''}>Graphs</button>
+        </div>
+    )
+}
+
+export default SensorHeader
--- webserver/frontend/hallway/src/routes/index.tsx
+++ webserver/frontend/hallway/src/routes/index.tsx
@@ -1,17 +1,33 @@
 import LatestTable from '../LatestTable';
 import ErrorTable from '../ErrorTable';
+import SensorHeader from '../SensorHeader';
 import GroupedMetricCharts from '../GroupedMetricCharts';
 import { createFileRoute } from '@tanstack/react-router'
-import React from 'react';
+import { type FC, useState } from 'react';
 
-const Sensors: React.FC = () => {
+function selectTab(tab: string) {
+    switch (tab) {
+        case 'live':
+            return <LatestTable />;
+        case 'errors':
+            return <ErrorTable />;
+        case 'graphs':
+            return <GroupedMetricCharts />;
+    }
+    return <></>;
+}
+
+const Sensors: FC = () => {
+    const [tab, setTab] = useState('live');
+
+    function handleClick(tabName: string) {
+        setTab(tabName);
+    }
+
     return (
         <div style={{ maxWidth: 1200, margin: 'auto', padding: 20 }}>
-            <LatestTable />
-            <br />
-            <ErrorTable />
-            <br />
-            <GroupedMetricCharts />
+            <SensorHeader tab={tab} onClick={handleClick} />
+            {selectTab(tab)}
         </div>
     );
 }
